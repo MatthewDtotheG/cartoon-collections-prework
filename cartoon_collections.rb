eb8207921e2910cb0016c7959be93f118abf32e3
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-12 01:28:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(calls)# code an argument here
 end
 
 def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w(gouda cheddar camembert)
+  cheeses = ["gouda", "cheddar", "camembert"]
 
   potentially_cheesy_items.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
