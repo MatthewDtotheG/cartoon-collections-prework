4ef6871f3a40c700269e353ce5270b74f3c33415
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-12 01:19:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ def find_the_cheese(cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.map { |cheese|
   cheese_types.include?(cheese)
-    puts cheese
+    return cheese
   }
 end
