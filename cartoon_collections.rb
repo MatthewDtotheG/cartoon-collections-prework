0d5d6583fc7bba0c547c4cfb3d6c389be4408c9c
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-12 01:18:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.include(cheese_types)
+  cheese.include?(cheese_types)
 
   }
 end
